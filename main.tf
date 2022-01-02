2aab32c0d52893151c9d722bc9a3536e651d4f87
manis <manish.kala2004@outlook.com>
2022-01-02 21:28:04 +0530

    added variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIA5D5JWOUCU3C6BY5J"
-  secret_key = "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"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_access_key" {
+  description = "The AWS region to use to create resources."
+  default     = "AKIA5D5JWOUCU3C6BY5J"
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key to use to create resources."
+  default     = "uTWrTSbV968h8/DfSqCojNG4HGDUBVYi7ekiwzNA"
+}
+
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
