992988ed5ee9b7f317b3c479096a73c330dbfc78
manis <manish.kala2004@outlook.com>
2022-01-02 19:45:26 +0530

    added region and access key

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = var.aws_region
+  access_key = "AKIA5D5JWOUCU3C6BY5J"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "aws_region" {
   description = "The AWS region to use to create resources."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-    default     = "my-s3bucket-"
+    default     = "my-s3bucket-POC4"
 }
 
 variable "tags" {
